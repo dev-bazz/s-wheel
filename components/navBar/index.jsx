cc0be4b7054581473ce86a7d4ff13f37413f8f07
BaZz <bazuaye50@gmail.com>
2023-11-20 11:29:46 +0100

    style: hero section  for reward site

--- components/navBar/index.jsx
+++ components/navBar/index.jsx
@@ -1,7 +1,8 @@
+import style from "./style.module.scss";
 const NavBar = () => {
 	return (
-		<nav>
-			<div className="">
+		<nav className={style.nav}>
+			<div className={style.container}>
 				<span className="">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
--- modules/home/index.jsx
+++ modules/home/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import style from "./style.module.scss";
+import heroImage from "../../public/reward_hero.png";
+import Image from "next/image";
 
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<>
+			<section
+				className={style.hero}
+				aria-labelledby="hero-title"
+			>
+				<div className={style.container}>
+					<div className={style.hero_content}>
+						<h1
+							className={style.hero_title}
+							id="hero-title"
+						>
+							Quickteller Customer Reward Program
+						</h1>
+						<p className={style.hero_text}>
+							Unlock the opportunity to win big by Playing with your
+							unique reference code.
+						</p>
+						<button className={style.button}>Transact Now</button>
+					</div>
+					<div className="">
+						<Image
+							src={heroImage}
+							width={520}
+							height={480}
+						/>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
